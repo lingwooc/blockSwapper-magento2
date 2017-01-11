eef60259f963f7ab965174d0d110f705ed351a62
Chris Lingwood <chris@thousandmonkeys.com>
2017-01-11 13:39:21 +0000

    support back button

--- app/code/thousandmonkeys/BlockSwap/view/frontend/web/js/swapper.js
+++ app/code/thousandmonkeys/BlockSwap/view/frontend/web/js/swapper.js
@@ -5,10 +5,17 @@ define([
 	'use strict'
 
 	return function (visible, hide) {
-		$('#'.hide).hide();
+		$('#'+hide).hide();
 		$('#swapper_action').click(function () {
 			$('#'+hide).show();
 			$('#'+visible).hide();
+			window.history.pushState({hmm:'show'}, "show", '#show');
+
 		})
+
+		$(window).on('popstate', function(event) {
+			$('#'+visible).show();
+			$('#'+hide).hide();
+		});
 	}
 })
